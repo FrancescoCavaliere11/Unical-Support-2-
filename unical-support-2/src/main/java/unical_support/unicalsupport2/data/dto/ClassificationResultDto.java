effcc1a88da034498de18780bce9fd60db002e36
lollo3393 <pndlnz03l11g791q@studenti.unical.it>
2025-11-03 00:31:22 +0100

    Aggiunti test per il classificatore, e rivisti

--- unical-support-2/src/main/java/unical_support/unicalsupport2/data/dto/ClassificationResultDto.java
+++ unical-support-2/src/main/java/unical_support/unicalsupport2/data/dto/ClassificationResultDto.java
@@ -3,7 +3,6 @@ package unical_support.unicalsupport2.data.dto;
 
 import lombok.Getter;
 
-@Getter
 // Classe in cui vengono definiti i nomi delle categorie possibili, e come deve essere formattato il risultato: categoria- confidenza- spiegazione
 public class ClassificationResultDto {
 
