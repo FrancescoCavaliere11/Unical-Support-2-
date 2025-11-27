1748e308318443bd2b5224e2b304a8353264093c
Riccardo-27 <lavoratoriccardo@gmail.com>
2025-11-27 11:25:05 +0100

    aggiunta suddivisione delle strategy

--- unical-support-2/src/main/java/unical_support/unicalsupport2/prompting/PromptService.java
+++ unical-support-2/src/main/java/unical_support/unicalsupport2/prompting/PromptService.java
@@ -12,25 +12,37 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class PromptService {
-    private final PromptStrategyFactory promptStrategyFactory;
 
-    @Getter
-    @Setter
-    private String currentStrategy = "fewShot";
+    private final PromptStrategyFactory factory;
+
+    @Getter @Setter
+    private String classifyCurrentStrategy = null;
+
+    @Getter @Setter
+    private String judgeCurrentStrategy = null;
+
+    @Getter @Setter
+    private String responderCurrentStrategy = null;
 
 
-    public String buildClassifyPrompt(List<ClassificationEmailDto> classificationEmailDtos) {
-        PromptStrategy strategy = promptStrategyFactory.getStrategy(currentStrategy);
-        return strategy.buildClassifyPrompt(classificationEmailDtos);
+    private String resolve(String module, String moduleCurrentStrategy) {
+        return moduleCurrentStrategy;
     }
-    public String buildJudgePrompt(List<ClassificationEmailDto> emails,
-                                   List<ClassificationResultDto> results) {
-        PromptStrategy strategy = promptStrategyFactory.getStrategy(currentStrategy);
+
+    public String buildClassifyPrompt(List<ClassificationEmailDto> emails) {
+        var strategy = factory.getStrategy("classifier", resolve("classifier", classifyCurrentStrategy));
+        return strategy.buildClassifyPrompt(emails);
+    }
+
+    public String buildJudgePrompt(List<ClassificationEmailDto> emails, List<ClassificationResultDto> results) {
+
+        var strategy = factory.getStrategy("judger", resolve("judger", judgeCurrentStrategy));
         return strategy.buildJudgePrompt(emails, results);
     }
 
-    public String buildResponderPrompt(List<unical_support.unicalsupport2.data.dto.classifier.ClassificationResultDto> emails) {
-        PromptStrategy strategy = promptStrategyFactory.getStrategy(currentStrategy);
+    public String buildResponderPrompt(List<ClassificationResultDto> emails) {
+
+        var strategy = factory.getStrategy("responder", resolve("responder", responderCurrentStrategy));
         return strategy.buildResponderPrompt(emails);
     }
 }
