5f600c2cdbfc4220e77c0dce3de4fba9abe44923
aldogioia <aldogioia2002@gmail.com>
2025-12-06 12:17:43 +0100

    Fixato l'orchestrator, ora le email con bassa confidence o non classificate vengono salvate

--- unical-support-2/src/main/java/unical_support/unicalsupport2/service/implementation/OrchestratorServiceImpl.java
+++ unical-support-2/src/main/java/unical_support/unicalsupport2/service/implementation/OrchestratorServiceImpl.java
@@ -133,20 +133,18 @@ public class OrchestratorServiceImpl implements OrchestratorService {
             System.out.println("--------------------------------------------------");
             System.out.printf("Email ID: %d | Destinatario: %s%n", r.getEmailId(), original.getTo());
 
-            boolean isLowConfidence = classification.getCategories().stream()
-                    .anyMatch(c -> c.getConfidence() <= 0.7);
+//            boolean isLowConfidence = classification.getCategories().stream()
+//                    .anyMatch(c -> c.getConfidence() <= 0.7);
 
             boolean isNonRiconosciuta = classification.getCategories().stream()
                     .anyMatch(c -> "NON RICONOSCIUTA".equalsIgnoreCase(c.getCategory()));
 
-            if (isLowConfidence || isNonRiconosciuta) {
-                emailService.saveEmailWithLowConfidence(original, classification);
-                if (isNonRiconosciuta) {
+            if (isNonRiconosciuta) { // isLowConfidence ||
+                    emailService.saveEmailWithLowConfidence(original, classification);
+                //if (isNonRiconosciuta) {
                     System.out.println(ANSI_YELLOW + "  EMAIL NON RICONOSCIUTA -> INOLTRO ALL'OPERATORE" + ANSI_RESET);
-                }
+                //}
             }
-
-
 
             if (r.getResponses() != null) {
                 for (SingleResponseDto response : r.getResponses()) {
