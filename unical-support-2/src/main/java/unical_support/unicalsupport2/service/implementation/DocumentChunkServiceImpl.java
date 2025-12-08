983374899391489405975c53c88da906b9b93b99
Tartaglia Francesco <148764037+gitCiccio@users.noreply.github.com>
2025-12-08 18:36:16 +0100

    Agginto soglia di similarità di 0.6 e spostata la query di ricerca dei chunk in DocumentChunkRepository

--- unical-support-2/src/main/java/unical_support/unicalsupport2/service/implementation/DocumentChunkServiceImpl.java
+++ unical-support-2/src/main/java/unical_support/unicalsupport2/service/implementation/DocumentChunkServiceImpl.java
@@ -20,6 +20,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DocumentChunkServiceImpl implements DocumentChunkService {
 
+    // Soglia di similarità (cosine distance)
+    private static final double MAX_COSINE_DISTANCE = 0.6; 
     private final DocumentChunkRepository chunkRepository;
     private final LlmClient llmClient;
     private final JdbcTemplate jdbcTemplate;
@@ -45,26 +47,42 @@ public class DocumentChunkServiceImpl implements DocumentChunkService {
             String embeddingString = gson.toJson(embedding);
 
             // 2. Query SQL
-            String sql = """
+            /*String sql = """
                 SELECT c.id 
                 FROM chunks c
                 JOIN documents d ON c.document_id = d.id
                 JOIN categories cat ON d.category_id = cat.id
                 JOIN chunk_embeddings ce ON ce.chunk_id = c.id
                 WHERE LOWER(cat.name) = LOWER(?)
+                AND ce.embedding <=> ?::vector <= ?
                 ORDER BY ce.embedding <=> ?::vector
                 LIMIT ?
-            """;
+            """;*/
+            //AND ce.embedding <=> ?::vector <= ?  --> filtro per soglia di similarità
+            //Accette solo chunl di distanza <= valore soglia
 
-            List<String> chunkIds = jdbcTemplate.query(sql,
+            List<String> chunkIds = chunkRepository.findRelevantChunkIds(
+                cat.getCategory(), 
+                embeddingString, 
+                MAX_COSINE_DISTANCE, 
+                kPerCategory
+            );
+            
+            /*jdbcTemplate.query(sql,
                     (ps) -> {
-                        ps.setString(1, cat.getCategory());
-                        ps.setString(2, embeddingString);
-                        ps.setInt(3, kPerCategory);
+                        ps.setString(1, cat.getCategory()); // WHERE LOWER(cat.name) = LOWER(?)
+                        ps.setString(2, embeddingString); // AND ce.embedding <=> ?::vector <= ?
+                        ps.setDouble(3, MAX_COSINE_DISTANCE); // soglia di similarità
+                        ps.setString(4, embeddingString); // ORDER BY ce.embedding <=> ?::vector
+                        ps.setInt(5, kPerCategory); // LIMIT ?
                     },
                     (rs, rowNum) -> rs.getString("id")
-            );
-
+            );*/
+            // In questo modo per calcolare la distanza del coseno
+            // Esculde i chunk che sono oltre la soglia di similarità
+            // Ordina i rimanenti per distanza crescente
+            // Ne prende al massimo kPerCategory
+            
             if (!chunkIds.isEmpty()) {
                 List<DocumentChunk> chunks = chunkRepository.findAllById(chunkIds);
 
