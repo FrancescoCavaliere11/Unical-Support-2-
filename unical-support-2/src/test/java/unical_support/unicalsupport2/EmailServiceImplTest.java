716c927f5b5be42d6eed9b8c9baa6e7e7675f94d
FrancescoCavaliere11 <francescounical@gmail.com>
2025-12-13 16:01:41 +0100

    modificato EmailServiceImplTest

--- unical-support-2/src/test/java/unical_support/unicalsupport2/EmailServiceImplTest.java
+++ unical-support-2/src/test/java/unical_support/unicalsupport2/EmailServiceImplTest.java
@@ -9,10 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import unical_support.unicalsupport2.data.dto.responder.ResponderResultDto;
 import unical_support.unicalsupport2.data.dto.responder.SingleResponseDto;
 import unical_support.unicalsupport2.data.entities.Answers;
-import unical_support.unicalsupport2.data.entities.Category;
 import unical_support.unicalsupport2.data.entities.Email;
 import unical_support.unicalsupport2.data.entities.Template;
-import unical_support.unicalsupport2.data.repositories.CategoryRepository;
 import unical_support.unicalsupport2.data.repositories.EmailRepository;
 import unical_support.unicalsupport2.data.repositories.TemplateRepository;
 import unical_support.unicalsupport2.service.implementation.EmailServiceImpl;
@@ -30,8 +28,7 @@ class EmailServiceImplTest {
 
     @Mock
     private EmailRepository emailRepository;
-    @Mock
-    private CategoryRepository categoryRepository;
+
     @Mock
     private TemplateRepository templateRepository;
 
@@ -43,12 +40,6 @@ class EmailServiceImplTest {
         String emailIdStr = "100";
         Email mockEmail = new Email();
         mockEmail.setId(emailIdStr);
-
-        // when(emailRepository.findById(emailIdStr)).thenReturn(Optional.of(mockEmail));
-
-        Category mockCategory = new Category();
-        mockCategory.setName("Tasse");
-        when(categoryRepository.findByNameIgnoreCase("Tasse")).thenReturn(Optional.of(mockCategory));
 
         Template mockTemplate = new Template();
         mockTemplate.setName("Standard");
@@ -78,8 +69,6 @@ class EmailServiceImplTest {
         var singleAnswer = answers.getSingleAnswers().getFirst();
 
         assertEquals("Testo risposta", singleAnswer.getAnswer());
-        assertEquals(0.95, singleAnswer.getReason(), 0.001);
-        assertEquals(mockCategory, singleAnswer.getCategory());
         assertEquals(mockTemplate, singleAnswer.getTemplate());
     }
 
@@ -99,22 +88,5 @@ class EmailServiceImplTest {
         Answers answers = emailCaptor.getValue().getAnswers();
         assertFalse(answers.getAnswered(), "Se non ci sono risposte, answered deve essere false");
         assertTrue(answers.getSingleAnswers().isEmpty());
-    }
-
-    @Test
-    void testSaveResponderResult_CategoryNotFound() {
-        String emailIdStr = "100";
-        Email mockEmail = new Email();
-        mockEmail.setId(emailIdStr);
-
-        when(categoryRepository.findByNameIgnoreCase("CategoriaInesistente")).thenReturn(Optional.empty());
-
-        SingleResponseDto responseDto = new SingleResponseDto(
-                "CategoriaInesistente", "Tmpl", "Content", null, "1.0"
-        );
-        ResponderResultDto resultDto = new ResponderResultDto(1, List.of(responseDto));
-
-        RuntimeException ex = assertThrows(RuntimeException.class, () -> emailService.saveAnswers(mockEmail, resultDto));
-        assertTrue(ex.getMessage().contains("Category not found"));
     }
 }
