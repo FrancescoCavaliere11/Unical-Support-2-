2c6a4af7f10fa0b0def3e6791ff0379f5e6e726e
Francesco Cavaliere <138306322+FrancescoCavaliere11@users.noreply.github.com>
2025-12-16 11:45:49 +0100

    Add CreationTimestamp to Document entity
    Added CreationTimestamp annotation to track document creation time.

--- unical-support-2/src/main/java/unical_support/unicalsupport2/data/entities/Document.java
+++ unical-support-2/src/main/java/unical_support/unicalsupport2/data/entities/Document.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
 
 import java.util.List;
 
